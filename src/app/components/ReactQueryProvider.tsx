fa94311a1a77ff7334578efc1bffc0a6776848bd
Kasey Bourdier <kasey@Kaseys-MacBook-Pro.local>
2026-03-03 16:44:33 -0500

    Updated ReactQueryProvider to not rerender and added prisma schema

--- src/app/components/ReactQueryProvider.tsx
+++ src/app/components/ReactQueryProvider.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-type Props = {
-  children: ReactNode;
-};
-
-export default function ReactQueryProvider({ children }: Props) {
-  const queryClient = new QueryClient();
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Use state to initialize the client only once per session
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
